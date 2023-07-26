07fe1d245ff19bc6f06b6ba1e2a903f59ed82b3c
Rafael Bertasso Borges <125775011+RafaelBertasso@users.noreply.github.com>
2023-07-26 13:32:10 -0300

    exercicios novos

---
+++ fundamentos/início/dados002.js
@@ -0,0 +1,17 @@
+//como criar variáveis em JavaScript
+// var/let nomeVar;
+//var/let nomeVar = valor;
+
+let preco = 19.90;
+let desconto = 0.4;
+
+//console.log(19.90 * 0.6);
+
+let precoComDesconto = preco * (1 - desconto);
+
+console.log(precoComDesconto);
+console.log(preco * (1 - desconto));
+
+var nome = "Caderno ";
+var categoria = "Papelaria";
+console.log(nome + categoria);
---
+++ fundamentos/início/dados003.js
@@ -0,0 +1,15 @@
+//tipos de dados
+//comando para mostrar o tipo do dado = typeof 
+
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4371.96;
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo);
+
+console.log(typeof "Teste");
+console.log(typeof 'Teste');
