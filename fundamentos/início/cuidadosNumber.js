96e6f45281047daf5131128ed91ff03278f06c0d
Rafael Bertasso Borges <125775011+RafaelBertasso@users.noreply.github.com>
2023-08-01 15:39:53 -0300

    novo

---
+++ fundamentos/início/cuidadosNumber.js
@@ -0,0 +1,6 @@
+console.log(7/0) // gera o infinito
+console.log('20' / 2) //Valor numerico inteiro dentro de string é aceito
+// valor numerico real não é aceito em uma string
+console.log('show' * 2) // gera um NaN (not a number)
+console.log(0.1 + 0.2)
+//console.log(10.toString)
---
+++ fundamentos/início/math.js
@@ -0,0 +1,7 @@
+const raio = 5
+const area = Math.PI * Math.pow(raio, 2)
+//Math.PI --> valor de PI
+//Math.pow --> potenciação .pow(nomeVar, exponente)
+
+console.log(area)
+console.log(typeof Math)
